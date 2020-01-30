cac8aca2ef7daa997af21d6b18f12b4d9cacaa3b
Quylan <quylanmac@gmail.com>
2020-01-30 06:42:08 +0000

    Test

--- test/test_DictionaryTrie.cpp
+++ test/test_DictionaryTrie.cpp
@@ -23,5 +23,10 @@ TEST(DictTrieTests, EMPTY_TEST) {
     DictionaryTrie dict;
     ASSERT_EQ(dict.find("abrakadabra"), false);
 }
-
+TEST(DictTrieTests, TEST_1) {
+    DictionaryTrie dict;
+    ASSERT_TRUE(dict.insert("food", 1));
+    //   ASSERT_FALSE(dict.insert("food", 1));
+    ASSERT_TRUE(dict.find("food"));
+}
 /* TODO */
