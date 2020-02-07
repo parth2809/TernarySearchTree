8f2c7fc152740f51b4355156addb7e4d5246870e
Parth Shah <parthshah2809@gmail.com>
2020-02-07 06:05:18 +0000

    did some commenting

--- src/DictionaryTrie/DictionaryTrie.cpp
+++ src/DictionaryTrie/DictionaryTrie.cpp
@@ -1,7 +1,8 @@
 /**
- * TODO: File Header
- *
- * Author:
+ * This is the Dictionary Trie cpp file. It has the implementation for all the
+ * methods for the dictionary trie class in the hpp file.
+ * Authors: Parth Shah,
+ * Quylan Mac Sources used: CSE100 PA2 write-up, Stepik Textbook
  */
 #include "DictionaryTrie.hpp"
 #include <algorithm>
@@ -10,168 +11,181 @@
 #include <string>
 #include <utility>
 using namespace std;
-
+// Initialized a pririoty queue to store pairs. Used in PredictCompletions
 typedef std::priority_queue<pair<string, int>*, vector<pair<string, int>*>,
                             Tcompare>
     pq;
+// Initialized a pririoty queue to store pairs. Used in PredictUnderscores
 typedef std::priority_queue<pair<string, int>*, vector<pair<string, int>*>,
                             Compare>
     rpq;
-/* TODO */
-DictionaryTrie::DictionaryTrie() : root(nullptr), treeSize(0), treeHeight() {
+
+/**
+ * Default constructor.
+ * Initialize an empty DictionaryTrie where the root is a null ptr.
+ * Initialized the queues for the min and max heap
+ */
+DictionaryTrie::DictionaryTrie() : root(nullptr) {
     queue = new pq();
     rqueue = new rpq();
 }
 
-/* TODO */
+/**
+ * insert method.
+ * Inserts a new node in the TST.
+ * parameters - word - the word to be inserted in the trie (letters inserted
+ * as nodes)
+ *            - frequency - frequency of the word in the trie
+ */
 bool DictionaryTrie::insert(string word, unsigned int freq) {
+    // initialize variable position to get char from word
     int position = 0;
+    // checks if the root is null to create the root
     if (root == nullptr) {
         root = new TrieNode(word[position], freq, false);
     }
+    // get char from word using position
     char c = word[position];
+    // stores the root in a temporary pointer variable called node
     TrieNode* node = root;
 
     while (true) {
+        //// checks if char is less then the node's value to traverse right
         if (c < node->getVal()) {
+            // checks if the left child is not a null pointer to traverse left
             if (node->left != nullptr) {
+                // set node to its left child
                 node = node->left;
             } else {
+                // if the left child is a null ptr then create a node over there
                 node->left = new TrieNode(c, freq, false);
+                // set node to its left child
                 node = node->left;
+                // for loop to traverse to the middle child of the nodes
+                // in order to store a new word in the TST
                 for (int i = position + 1; i < word.length(); i++) {
+                    // creates new node as the middle child of node
                     node->middle = new TrieNode(word[i], freq, false);
+                    // sets node to its midddle child
                     node = node->middle;
                 }
+                // at the end of the word sets the boolean of end word to true
                 node->setFinalLetter(true);
-                break;
+                return true;
             }
+            //// checks if char is less then the node's value to traverse right
         } else if (c > node->getVal()) {
+            // checks if the right child is not a null pointer to traverse right
             if (node->right != nullptr) {
+                // set node to its right child
                 node = node->right;
             } else {
+                // if the right child is a null ptr then create a node over
+                // there
                 node->right = new TrieNode(c, freq, false);
+                // set node to its right child
                 node = node->right;
+                // for loop to traverse to the middle child of the nodes
+                // in order to store a new word in the TST
                 for (int i = position + 1; i < word.length(); i++) {
+                    // creates new node as the middle child of node
                     node->middle = new TrieNode(word[i], freq, false);
+                    // sets node to its midddle child
                     node = node->middle;
                 }
+                // at the end of the word sets the boolean of end word to true
                 node->setFinalLetter(true);
-                break;
+                return true;
             }
+            //// checks if char is the same as node's value to traverse down
         } else {
+            // checks if the position is equal to the length of the word
             if (position == word.length() - 1) {
+                // sets the node to a word node
                 node->setFinalLetter(true);
+                // gives frequency to the node
                 node->setFreq(freq);
                 return true;
             } else {
+                // checks if the middle node is not null
                 if (node->middle != nullptr) {
+                    // makes node its middle child
                     node = node->middle;
+                    // increments position
                     ++position;
+                    // get new char from new position
                     c = word[position];
                 } else {
+                    // traverse down the middle child by forming new nodes
                     for (int i = position + 1; i < word.length(); i++) {
+                        // creates new nodes as middle child
                         node->middle = new TrieNode(word[i], freq, false);
+                        // makes node its middle child
                         node = node->middle;
                     }
+                    // set the boolean for the end of the word
                     node->setFinalLetter(true);
-                    break;
+                    return true;
                 }
             }
         }
     }
-}
-/*
-TrieNode* DictionaryTrie::insertChar(TrieNode* node, string word,
-                                     unsigned int freq) {
-    char c = word[0];
-    if (node == nullptr) {
-        node = new TrieNode(c, freq, false);
-    } else if (node->getVal() < c) {
-        node->right = insertChar(node->right, word, freq);
-    } else if (node->getVal() > c) {
-        node->left = insertChar(node->right, word, freq);
-    } else if (word.length() != 1) {
-        word.erase(word[0]);
-        node->middle = insertChar(node->right, word, freq);
-    } else {
-        node->setFinalLetter(true);
-        node->setFreq(freq);
-    }
-    return node;
+    return false;
 }
 
-char DictionaryTrie::nextCharacter(string word, int position) {
-    return word[position];
-}
-
-
-//TODO
-DictionaryTrie::TrieNode* DictionaryTrie::insertLetterNode(
-    TrieNode* node, char c, unsigned int freq, int position, string word) {
-    TrieNode* temp = node;
-    // Current node does not exist
-    if (temp == nullptr) {
-        // Reached last char of input word and exits function
-        if (position == word.length() - 1) {
-            temp->setVal(c);
-            temp->setFreq(freq);
-            temp->setFinalLetter(true);
-            return temp;
-        }
-        temp->setVal(c);
-        temp->setFreq(freq);
-        temp->setFinalLetter(false);
-        position++;
-        temp->middle = insertLetterNode(
-            temp->middle, nextCharacter(word, position), freq, position,
-word); } else if (temp->getVal() < c) { temp->right =
-insertLetterNode(temp->right, c, freq, position, word); } else if
-(temp->getVal() > c) { temp->left = insertLetterNode(temp->left, c, freq,
-position, word); } else if (temp->getVal() == c) {
-        // Found last letter of word
-        if (position == word.length() - 1) {
-            return nullptr;
-        }
-        position++;
-        temp->middle = insertLetterNode(
-            temp->middle, nextCharacter(word, position), freq, position,
-word);
-    }
-    return temp;
-    ;
-}
-*/
-/* TODO */
+/**
+ * find method.
+ * returns a boolean for whether the word exists in the tree
+ * parameters - word - the word that needs to be looked for in the trie
+ */
 bool DictionaryTrie::find(string word) const {
+    // checks if trie is empty
     if (root == nullptr) {
         return false;
     }
+    // store the root in a new node
     TrieNode* node = root;
+    // create position to get next character in a word
     int position = 0;
+    // gets char using the position
     char c = word[position];
 
     while (true) {
+        // checks if char is smaller the value of the node
         if (c < node->getVal()) {
+            // the left child is not null
             if (node->left != nullptr) {
+                // sets node to the left child
                 node = node->left;
+                // if it is null it returns false
             } else {
                 return false;
             }
+            // checks if char is greater the value of the node
         } else if (c > node->getVal()) {
+            // the right child is not null
             if (node->right != nullptr) {
+                // sets node to the right child
                 node = node->right;
+                // if it is null it returns false
             } else {
                 return false;
             }
+            // checks if char is equal to the value of the node
         } else {
+            // checks if the position is equal to the length of the word
+            // and if the node is an end letter.
             if (position == word.length() - 1 &&
                 node->getFinalLetter() == true) {
                 return true;
             } else {
+                // checks if the middle child is not null
                 if (node->middle != nullptr) {
+                    // makes the node the middle child
                     node = node->middle;
+                    // increment position
                     ++position;
+                    // char at the position of the word
                     c = word[position];
                 } else {
                     return false;
@@ -179,6 +193,7 @@ bool DictionaryTrie::find(string word) const {
             }
         }
     }
+    return false;
 }
 
 TrieNode* DictionaryTrie::findNode(string word) const {
@@ -235,15 +250,6 @@ void DictionaryTrie::traversal(TrieNode* node, string prefix,
                 // Pushes the pair into the queue
                 queue->push(pair1);
             } else {
-                /* pair<string, int>* pair1 = new pair<string, int>();
-                 int value = node->getFreq();
-                 pair1->first = prefix + node->getVal();
-                 pair1->second = value;
-                 // Pushes the pair into the queue
-                 queue->push(pair1);
-                 pair<string, int>* hold = queue->top();
-                 queue->pop();
-                 delete (hold);*/
                 // Current node greater than min freq in queue
                 if (node->getFreq() > queue->top()->second) {
                     pair<string, int>* pair1 = new pair<string, int>();
--- src/DictionaryTrie/DictionaryTrie.hpp
+++ src/DictionaryTrie/DictionaryTrie.hpp
@@ -1,8 +1,10 @@
 /**
- * TODO: File Header
- *
- * Author:
+ * This is the Dictionary Trie hpp file. It has the classes for a trieNode and
+ * DictionaryTrie along with structs for the priority queues.
+ * Authors: Parth Shah, Quylan Mac
+ * Sources used: CSE100 PA2 write-up, Stepik Textbook
  */
+
 #ifndef DICTIONARY_TRIE_HPP
 #define DICTIONARY_TRIE_HPP
 
@@ -12,11 +14,14 @@
 #include <vector>
 
 using namespace std;
-/**
- * The class for a dictionary ADT, implemented as either
- * a mulit-way trie or a ternary search tree.
- */
 
+/**
+ * This is a Ternary Search Tree Node class called TrieNode, which has a
+ * character value (its value), a frequency, and a boolean signifying if it is a
+ * end word or not. In addition, it has a pointer to its left child, right
+ * child, and the middle child. It also has the following methods: setVal,
+ * getVal, setFreq, getFreq, setFinalLetter and getFinalLetter.
+ */
 class TrieNode {
   private:
     int frequency;
@@ -27,23 +32,52 @@ class TrieNode {
     TrieNode* left;
     TrieNode* middle;
     TrieNode* right;
-
+    /**
+     * Default constructor.
+     * Initialize an empty TST Node, with its right child, left child, and
+     * middle child are null.
+     */
     TrieNode() : value('\0'), frequency(0), endWord(false) {
         left = middle = right = nullptr;
     }
-
+    /**
+     * Constructor.
+     * Initialize an empty TST Node, with its right child, left child, and
+     * middle child are null.
+     * parameters - c - the character value of the node
+     *            - a - the frequency at the node
+     *            - b - boolean for whether the node is the end of the word
+     */
     TrieNode(const char& c, const int& a, const bool& b)
         : value(c), frequency(a), endWord(b) {
         left = middle = right = nullptr;
     }
+    /** Set the value of parameter value
+     * Paramater: c - the character to set the node with
+     */
     void setVal(const char& c) { value = c; }
+    /** returns the value of the parameter value */
     char getVal() { return value; }
+    /** Set the value of frequency
+     * Paramater: a - the integer to set the frequency of the node with
+     */
     void setFreq(const int& a) { frequency = a; }
+    /** returns the value of the frequency of the node */
     int getFreq() { return frequency; }
+    /** Set the value of the boolean showing whether is it the final letter
+     * Paramater: b - the boolean to set the node with to check if the node ends
+     * in a word
+     */
     void setFinalLetter(const bool& b) { endWord = b; }
+    /** returns the boolean value which depends of whether the node is the end
+     * of a word*/
     bool getFinalLetter() { return endWord; }
 };
-
+/**
+ * This is a struct that contains a function compare which compares two pairs,
+ * i.e. compares its frequency first and then the alphabetical ordering of its
+ * string. It is used to create a max heap
+ */
 struct Tcompare {
     bool operator()(pair<string, int>* w1, pair<string, int>* w2) const {
         // Frequencies not equal
@@ -55,6 +89,11 @@ struct Tcompare {
         return w1->first < w2->first;
     }
 };
+/**
+ * This is a struct that contains a function compare which compares two pairs,
+ * i.e. compares its frequency first and then the alphabetical ordering of its
+ * string. It is used to create a min heap
+ */
 struct Compare {
     bool operator()(pair<string, int>* w1, pair<string, int>* w2) const {
         // Frequencies not equal
@@ -66,46 +105,117 @@ struct Compare {
         return w1->first > w2->first;
     }
 };
+// Initialized a pririoty queue to store pairs. Used in PredictCompletions
 typedef std::priority_queue<pair<string, int>*, vector<pair<string, int>*>,
                             Tcompare>
     pq;
+// Initialized a pririoty queue to store pairs. Used in PredictUnderscores
 typedef std::priority_queue<pair<string, int>*, vector<pair<string, int>*>,
                             Compare>
     rpq;
+
+/**
+ * This is DictionaryTrie class, which has a root and two queues ( one for a min
+ * heap and one for a max heap, since theyre used by predictCompletions and
+ * predict Underscore respctively). It also has the
+ * following methods: insert, findNode, traversal, find, predictCompletions,
+ * predictUnderscores, traverselength, recurse and deleteAll.
+ */
 class DictionaryTrie {
   private:
     TrieNode* root;
-    int treeHeight;
-    int treeSize;
     pq* queue;
     rpq* rqueue;
 
   public:
-    /* TODO: add function header */
+    /**
+     * Default constructor.
+     * Initialize an empty DictionaryTrie
+     */
     DictionaryTrie();
 
-    /* TODO: add function header */
+    /**
+     * insert method.
+     * Inserts a new node in the TST.
+     * parameters - word - the word to be inserted in the trie (letters inserted
+     * as nodes)
+     *            - frequency - frequency of the word in the trie
+     */
     bool insert(string word, unsigned int freq);
-
+    /**
+     * findNode method.
+     * returns the pointer to a node that is the last letter of a word.
+     * parameters - word - the word that needs to be looked for in the trie
+     */
     TrieNode* findNode(string word) const;
-
+    /**
+     * traversal method.
+     * method that performs a depth first search on the trie. used to fill the
+     * priority queue
+     *  parameters - node -the node where you begin the traversal
+     *             - prefix - the prefix which is used to get all words
+     *             - numCompletions - denotes the maximum number of completions
+     * that need to be returned
+     */
     void traversal(TrieNode* node, string prefix, int numCompletions);
 
-    /* TODO: add function header */
+    /**
+     * find method.
+     * returns a boolean for whether the word exists in the tree
+     * parameters - word - the word that needs to be looked for in the trie
+     */
     bool find(string word) const;
 
-    /* TODO: add function header */
+    /**
+     * predictCompletions method.
+     * returns a vector with the the number of completions required that have
+     * the highest frequency
+     * parameters - prefix - the prefix which is used to get all words
+     *            - numCompletions - denotes the maximum number of completions
+     */
     vector<string> predictCompletions(string prefix,
                                       unsigned int numCompletions);
 
-    /* TODO: add function header */
+    /**
+     * predictCompletions method.
+     * returns a vector with the the number of completions required that have
+     * the highest frequency and can be produced from the pattern
+     * parameters - pattern - the pattern with underscores that is being
+     * searched for
+     *            - numCompletions - denotes the maximum number of completions
+     */
     vector<string> predictUnderscores(string pattern,
                                       unsigned int numCompletions);
-    void traversalLength(TrieNode* node, string word, int length);
-    bool recurse(string pattern, string word);
-    /* TODO: add function header */
-    ~DictionaryTrie();
 
+    /**
+     * traversalLrngth method.
+     * method that performs a depth first search on the trie. used to fill the
+     * priority queue with words of a fixed length
+     *  parameters - node -the node where you begin the traversal
+     *             - word - the prefix which is used to get all words
+     *             - length - the length of the words being needed
+     */
+    void traversalLength(TrieNode* node, string word, int length);
+
+    /**
+     * recurse method.
+     * method use to check if the word fits with the pattern perfectly
+     *  parameters - pattern - the pattern with underscores that is being
+     * searched for
+     *             - word - the prefix which is used to get all words
+     */
+    bool recurse(string pattern, string word);
+
+    /**
+     * Destructor.
+     * Deletes every node in the TST. Uses helper method deleteAll
+     */
+    ~DictionaryTrie();
+    /**
+     * A helper method for destructor. Recursively deletes every node in the
+     * tree. Deletes nothing if root is nullptr.
+     * Paramater: n - a pointer to the root of the tree.
+     */
     void deleteAll(TrieNode* n);
 };
 
